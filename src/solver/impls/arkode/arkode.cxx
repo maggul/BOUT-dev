595db6ff23e893c7f08f7305fa90c49204c0d32d
Ben Dudson <dudson2@llnl.gov>
2025-02-07 18:04:47 -0800

    ARKode solver: Use ARKodeGetNumRhsEvals from v7.2.0
    Previous use of ARKStepGetNumRhsEvals is deprecated from SUNDIALS
    7.2.0, replaced by ARKodeGetNumRhsEvals
    
    https://github.com/LLNL/sundials/blob/main/CHANGELOG.md#deprecation-notices

--- src/solver/impls/arkode/arkode.cxx
+++ src/solver/impls/arkode/arkode.cxx
@@ -528,13 +528,18 @@ int ArkodeSolver::run() {
 
     // Get additional diagnostics
     long int temp_long_int = 0;
-    long int temp_long_int = 0;
+    long int temp_long_int2 = 0;
     ARKodeGetNumSteps(arkode_mem, &temp_long_int);
     nsteps = int(temp_long_int);
-    ARKodeGetNumRhsEvals(arkode_mem, 0, &temp_long_int);
+#if SUNDIALS_VERSION_AT_LEAST(7, 2, 0)
+    ARKodeGetNumRhsEvals(arkode_mem, 0, &temp_long_int); // Explicit
+    ARKodeGetNumRhsEvals(arkode_mem, 1, &temp_long_int2); // Implicit
+#else
+    // This function was deprecated in 7.2.0
+    ARKStepGetNumRhsEvals(arkode_mem, &temp_long_int, &temp_long_int2);
+#endif
     nfe_evals = int(temp_long_int);
-    ARKodeGetNumRhsEvals(arkode_mem, 1, &temp_long_int);
-    nfi_evals = int(temp_long_int);
+    nfi_evals = int(temp_long_int2);
     if (treatment == Treatment::ImEx or treatment == Treatment::Implicit) {
       ARKodeGetNumNonlinSolvIters(arkode_mem, &temp_long_int);
       nniters = int(temp_long_int);
