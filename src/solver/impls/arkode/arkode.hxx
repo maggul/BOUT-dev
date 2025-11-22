2a6368b6a4d67594aaa5bd0bc4b1e9a8f1b72c7e
maggul <mustafaaggul06@gmail.com>
2025-11-21 23:15:41 -0600

    continuous temporal filtering

--- src/solver/impls/arkode/arkode.hxx
+++ src/solver/impls/arkode/arkode.hxx
@@ -44,6 +44,7 @@ RegisterUnavailableSolver
 #include "bout/bout_types.hxx"
 #include "bout/region.hxx"
 #include "bout/sundials_backports.hxx"
+#include "temporal_filtering.hxx"
 
 #include <nvector/nvector_parallel.h>
 #include <sundials/sundials_config.h>
@@ -182,6 +183,21 @@ private:
   bool use_jacobian;
   /// N_Vector backend to use
   NVectorType nvector_type;
+  /// Temporal filtering of solution
+  /// - Whether to use temporal filtering
+  bool use_temporal_filtering{false};
+  /// Temporal filtering object
+  TemporalFiltering temp_filtering;
+  /// Type of temporal filtering to use
+  FilteringType filtering_type{FilteringType::None};
+  /// Interval over which means are calculated
+  /// - EMA: time scale of the exponential memory
+  /// - SRA: window length of each running-average block
+  BoutReal tau_mean{0.0};
+  /// Time at which averaging starts
+  BoutReal mean_start_time{0.0};
+  /// Relaxation/nudging parameter for temporal filtering
+  BoutReal lambda{0.0};
 #if ARKODE_OPTIMAL_PARAMS_SUPPORT
   /// Use ARKode optimal parameters
   bool optimize;
@@ -204,7 +220,7 @@ private:
   SundialsNVectorInterface nvector_backend() {
     return SundialsNVectorInterface(*this, suncontext, nvector_type);
   }
-
+  void apply_temporal_filtering(BoutReal internal_time, N_Vector uvec);
   /// SPGMR solver structure
   SUNLinearSolver sun_solver{nullptr};
   /// Solver for implicit stages
