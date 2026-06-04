68d180112904dcab2901f4b944c057bab7559589
Mustafa Aggul <maggul@login35.chn.perlmutter.nersc.gov>
2026-06-04 11:09:33 -0700

    added error_bias

--- src/solver/impls/arkode/arkode.cxx
+++ src/solver/impls/arkode/arkode.cxx
@@ -137,6 +137,9 @@ ArkodeSolver::ArkodeSolver(Options* opts)
       mx_growth((*options)["mx_growth"]
               .doc("Maximum allowed growth factor in step size between consecutive steps")
               .withDefault(20.0)),
+      error_bias((*options)["error_bias"]
+              .doc("Bias factor to slightly exaggerate the temporal error")
+              .withDefault(1.0)),
       abstol((*options)["atol"].doc("Absolute tolerance").withDefault(1.0e-12)),
       reltol((*options)["rtol"].doc("Relative tolerance").withDefault(1.0e-5)),
       use_vector_abstol((*options)["use_vector_abstol"]
@@ -371,6 +374,11 @@ int ArkodeSolver::init() {
     throw BoutException("ARKodeSetAdaptivityMethod failed\n");
   }
 #endif
+
+  if (SUNAdaptController_SetErrorBias(controller, error_bias)
+      != ARK_SUCCESS) {
+    throw BoutException("SUNAdaptController_SetErrorBias failed\n");
+  }
 
   if (use_vector_abstol) {
     std::vector<BoutReal> f2dtols;
