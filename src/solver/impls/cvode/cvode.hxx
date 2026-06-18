d5619e245297ba5f49f5ba24c85401783b7dd784
Mustafa Aggul <maggul@login26.chn.perlmutter.nersc.gov>
2026-06-18 14:30:04 -0700

    print_allstats flag for CVODE

--- src/solver/impls/cvode/cvode.hxx
+++ src/solver/impls/cvode/cvode.hxx
@@ -161,6 +161,8 @@ private:
   NVectorType nvector_type;
   BoutReal cvode_nonlinear_convergence_coef;
   BoutReal cvode_linear_convergence_coef;
+  /// CVodePrintAllStats: print all available integrator statistics at the end of the run
+  bool print_allstats;
   /// Temporal filtering of solution
   /// - Whether to use temporal filtering
   bool use_temporal_filtering{false};
